03703054ba9771793fbb7eb877a536561ab3a45d
THEDECAL <thedecal2@gmail.com>
2019-01-16 17:25:28 +0200

    16 янв 2019 г. 17:25:26

---
+++ CSharp/WinFormsLeeson1/task3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MouseMove += Form1_MouseEvent;
+            this.MouseClick += Form1_MouseEvent;
+        }
+        private void Form1_MouseEvent(object sender, MouseEventArgs e)
+        {
+            Text = $"X{e.X.ToString()}:Y{e.Y.ToString()}";
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (Control.ModifierKeys == Keys.Control) this.Close();
+
+                string position = "";
+                int topBorder = 9, bottomBorder = ClientSize.Height - 19;
+                int leftBorder = 9, rightBorder = ClientSize.Width - 19;
+
+                if (e.X == leftBorder || e.X == rightBorder || e.Y == topBorder || e.Y == bottomBorder)
+                    position = "Курсор на границе прямоугольника";
+                else if (e.X < leftBorder || e.X > rightBorder || e.Y < topBorder || e.Y > bottomBorder)
+                    position = "Курсор снаружи прямоугольника";
+                else if (e.X > leftBorder || e.X < rightBorder || e.Y > topBorder || e.Y < bottomBorder)
+                    position = "Курсор внутри прямоугольника";
+
+                MessageBox.Show(position, "Нажата левая кнопка мыши", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (e.Button == MouseButtons.Right)
+                Text += $" W{ClientSize.Width}:H{ClientSize.Height}";
+        }
+    }
+}
---
+++ CSharp/WinFormsLeeson1/task4/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task4
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int clickDownX, clickDownY;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.MouseMove += Form1_MouseEvent;
+            this.MouseDown += Form1_MouseDown;
+            this.MouseUp += Form1_MouseUp;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e) { clickDownX = e.X; clickDownY = e.Y; }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (clickDownX + e.X < 10 || clickDownY + e.Y < 10)
+                MessageBox.Show("Минимальный размер прямоугольника 10х10.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                int r = random.Next(255);
+                int g = random.Next(255);
+                int b = random.Next(255);
+
+                Label square = new Label();
+                int Width = Math.Abs(clickDownX - e.X);
+                int Height = Math.Abs(clickDownY - e.Y);
+
+                square.Text = "";
+                square.Top = clickDownY;
+                square.Left = clickDownX;
+                square.BackColor = Color.FromArgb(r, g, b);
+                square.Size = new Size(Width, Height);
+                square.Tag = DateTime.Now;
+
+                Controls.Add(square);
+            }
+        }
+        private void Form1_MouseEvent(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Text = "Naz";
+                if (e.Button == MouseButtons.None)
+                    Text = "Nena";
+            }
+        }
+    }
+}
