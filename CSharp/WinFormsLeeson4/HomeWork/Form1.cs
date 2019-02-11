70c8e57dbb38685a6e7c856017df334115ce3de4
THEDECAL <thedecal2@gmail.com>
2019-02-11 16:50:34 +0200

    11 фев 2019 г. 16:50:33

--- CSharp/WinFormsLeeson4/HomeWork/Form1.cs
+++ CSharp/WinFormsLeeson4/HomeWork/Form1.cs
@@ -18,13 +18,27 @@ namespace HomeWork
         public Form1()
         {
             InitializeComponent();
+
             Init_cbSelectView();
-            cbSelectView.SelectedIndex = 2;
             UpdateView();
             UpdateListDisks();
             Init_lvList();
             tvTree.ImageList = InitImages(new Size(16, 16));
             ChangeDisk();
+        }
+        private void InitFileSystemWatcher(string path, dynamic parentNode)
+        {
+            //Отключаем старое наблюдение за ФС
+            if (fsw != null) fsw.EnableRaisingEvents = false;
+
+            fsw = new FileSystemWatcher(path);
+            Action updateList = () => { LoadTreeContent(path, parentNode); LoadListContent(path); };
+            fsw.Created += (s, e) => { updateList(); };
+            fsw.Deleted += (s, e) => { updateList(); };
+            fsw.Changed += (s, e) => { updateList(); };
+            fsw.Renamed += (s, e) => { updateList(); };
+            fsw.SynchronizingObject = tvTree;
+            fsw.EnableRaisingEvents = true;
         }
         private void UpdateListDisks() => cbSelectDisk.DataSource = Environment.GetLogicalDrives();
         private void UpdateView()
@@ -35,11 +49,19 @@ namespace HomeWork
                 lvList.View = (View)Enum.Parse(typeof(View), style);
             }
         }
-        private void Init_cbSelectView() => cbSelectView.Items.AddRange(Enum.GetNames(typeof(View)));
+        private void Init_cbSelectView()
+        {
+            cbSelectView.Items.AddRange(Enum.GetNames(typeof(View)));
+            cbSelectView.SelectedIndex = 3;
+        }
         private void ChangeDisk()
         {
             if (cbSelectDisk.SelectedIndex != -1)
-                LoadTreeContent(cbSelectDisk.SelectedItem as string);
+            {
+                tvTree.Nodes.Clear();
+                LoadTreeContent(cbSelectDisk.SelectedItem as string, tvTree);
+                tvTree.SelectedNode = null;
+            }
         }
         private void Init_lvList()
         {
@@ -52,45 +74,6 @@ namespace HomeWork
             lvList.Columns.Add("Имя");
             lvList.Columns.Add("Тип");
             lvList.Columns.Add("Размер");
-
-            if(cbSelectDisk.SelectedIndex != -1)
-                LoadListContent(cbSelectDisk.SelectedItem as string);
-        }
-        private void LoadListContent(string path)
-        {
-            try
-            {
-                if (Directory.Exists(path))
-                {
-                    var files = Directory.GetFiles(path);
-                    var dirs = Directory.GetDirectories(path);
-
-                    lvList.Items.Clear();
-                    if (files.Length > 0 || dirs.Length > 0)
-                    {
-                        foreach (var dirPath in dirs)
-                        {
-                            var d = new FileInfo(dirPath);
-                            ListViewItem lvi = new ListViewItem(d.Name);
-                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "Папка"));
-                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "-"));
-                            lvi.ImageIndex = 0;
-                            lvList.Items.Add(lvi);
-                        }
-
-                        foreach (var filePath in files)
-                        {
-                            var f = new FileInfo(filePath);
-                            ListViewItem lvi = new ListViewItem(f.Name);
-                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "Файл"));
-                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, f.Length.ToString()));
-                            lvi.ImageIndex = 2;
-                            lvList.Items.Add(lvi);
-                        }
-                    }
-                }
-            }
-            catch (UnauthorizedAccessException) { }
         }
         private ImageList InitImages(Size size)
         {
@@ -102,64 +85,110 @@ namespace HomeWork
 
             return iList;
         }
-        private void LoadTreeContent(string path, TreeNode node = null)
+        private void LoadListContent(string path)
         {
             try
             {
                 if (Directory.Exists(path))
                 {
-                    var dirsAndFiles = Directory.GetDirectories(path).ToList();
-                    //Искать файлы если снят флажок "Только папки"
-                    if (!chkSelectOnlyFolder.Checked) dirsAndFiles.AddRange(Directory.GetFiles(path).ToList());
+                    var dirs = Directory.GetDirectories(path);
+                    var files = Directory.GetFiles(path);
 
-                    //Переменная типа TreeView для корневого узла или типа TreeNode для дочерних узлов
-                    dynamic parentNode;
-                    if (node == null) parentNode = tvTree; //Если это корневой узел
-                    else parentNode = node; //Если это дочерний узел
-                    
-                    parentNode.Nodes.Clear();
-                    if (dirsAndFiles.Count > 0)
+                    lvList.Items.Clear();
+                    if (files.Length > 0 || dirs.Length > 0)
                     {
-                        foreach (var fullPath in dirsAndFiles)
+                        foreach (var dirPath in dirs)
                         {
-                            //Удаляем лишние символы для имени узла
-                            var newNode = new TreeNode(fullPath.Replace(path + $"{(node == null ? "" : "\\")}", ""));
-                            try
-                            {
-                                //Если это папка и в ней есть папки добавляем "плюсик" к узлу
-                                if (Directory.Exists(fullPath) && Directory.GetDirectories(fullPath).Length > 0)
-                                    newNode.Nodes.Add("");
-                                //Если это файл меняем иконку на иконку "файл"
-                                else if (File.Exists(fullPath)) newNode.ImageIndex = 2;
-                            }
-                            catch (UnauthorizedAccessException) { } //На случай недостатка прав на операцию
+                            var d = new FileInfo(dirPath);
+                            ListViewItem lvi = new ListViewItem(d.Name);
+                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "Папка"));
+                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "-"));
+                            lvi.ImageIndex = 0;
+                            lvi.Tag = dirPath;
+                            lvList.Items.Add(lvi);
+                        }
 
-                            newNode.Tag = fullPath;
+                        foreach (var filePath in files)
+                        {
+                            var f = new FileInfo(filePath);
+                            ListViewItem lvi = new ListViewItem(f.Name);
+                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, "Файл"));
+                            lvi.SubItems.Add(new ListViewItem.ListViewSubItem(lvi, f.Length.ToString()));
+                            lvi.ImageIndex = 2;
+                            lvi.Tag = filePath;
+                            lvList.Items.Add(lvi);
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+        private void LoadTreeContent(string path, dynamic parentNode)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    var dirs = Directory.GetDirectories(path);
+                    //Искать файлы если снят флажок "Только папки"
+                    var files = (!chkSelectOnlyFolder.Checked) ? Directory.GetFiles(path) : new string[0];
+
+                    tbCurrentPath.Text = path;
+                    LoadListContent(path);
+                    InitFileSystemWatcher(path, parentNode);
+                    parentNode.Nodes.Clear();
+
+                    if (dirs.Length > 0 || files.Length > 0)
+                    {
+                        foreach (var dirPath in dirs)
+                        {
+                            DirectoryInfo di = new DirectoryInfo(dirPath);
+                            var newNode = new TreeNode(di.Name);
+                            newNode.Nodes.Add("");
+                            newNode.Tag = dirPath;
+                            parentNode.Nodes.Add(newNode);
+                        }
+                        
+                        foreach (var filePath in files)
+                        {
+                            FileInfo fi = new FileInfo(filePath);
+                            var newNode = new TreeNode(fi.Name);
+                            //Меняем иконку на иконку "файл"
+                            newNode.ImageIndex = 2;
+                            newNode.Tag = filePath;
                             parentNode.Nodes.Add(newNode);
                         }
                     }
                 }
             }
-            catch (UnauthorizedAccessException) { } //На случай недостатка прав на операцию
+            catch (UnauthorizedAccessException) {} //На случай недостатка прав на операцию
         }
         private void cbSelectDisk_DropDown(object sender, EventArgs e) => UpdateListDisks();
 
-        private void chkSelectOnlyFolder_CheckedChanged(object sender, EventArgs e) => cbSelectDisk_SelectionChangeCommitted(null, null);
+        private void chkSelectOnlyFolder_CheckedChanged(object sender, EventArgs e) => ChangeDisk();
 
         private void cbSelectDisk_SelectionChangeCommitted(object sender, EventArgs e) => ChangeDisk();
 
         private void tvTree_BeforeExpand(object sender, TreeViewCancelEventArgs e)
         {
+            string path = e.Node.Tag as string;
+            tvTree.SelectedNode = e.Node;
+            try
+            {
+                if (chkSelectOnlyFolder.Checked &&
+                    Directory.GetDirectories(path).Length > 0) LoadTreeContent(path, e.Node);
+                else if (!chkSelectOnlyFolder.Checked &&
+                    Directory.GetFiles(path).Length > 0 ||
+                    Directory.GetDirectories(path).Length > 0) LoadTreeContent(path, e.Node);
+                else throw new UnauthorizedAccessException();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                e.Node.Nodes.Clear();
+                lvList.Items.Clear();
+                return;
+            }
             e.Node.ImageIndex = 1;
-            LoadTreeContent(e.Node.Tag as string, e.Node);
-        }
-
-        private void tvTree_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            if (Directory.Exists(e.Node.Tag as string))
-                LoadListContent(e.Node.Tag as string);
-            else if (File.Exists(e.Node.Tag as string))
-                Process.Start(e.Node.Tag as string);
         }
 
         private void tvTree_BeforeCollapse(object sender, TreeViewCancelEventArgs e)
@@ -173,8 +202,21 @@ namespace HomeWork
 
         private void lvList_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            var lvi = lvList.SelectedItems[0] as ListViewItem;
-            Process.Start(lvi.Tag as string);
+            string path = lvList.SelectedItems[0].Tag as string;
+            if (File.Exists(path)) Process.Start(path);
+        }
+
+        private void tvTree_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            string path = tvTree.SelectedNode.Tag as string;
+            if (File.Exists(path)) Process.Start(path);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            tvTree.SelectedNode = tvTree.SelectedNode?.Parent;
+            if (tvTree.SelectedNode == null) ChangeDisk();
+            else LoadTreeContent(tvTree.SelectedNode.Tag as string, tvTree.SelectedNode);
         }
     }
 }
