1e03aba1fbd11879aa17778e13531b1b546ab17e
PERFECT-PC <thedecal1@gmail.com>
2019-03-01 21:04:07 +0200

    1 мар 2019 г. 21:04:06

---
+++ CSharp/smartfon_catalog/Downloader.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartfon_catalog
+{
+    static class Downloader
+    {
+        static public HtmlDocument GetPage(string URL)
+        {
+            WebClient client = new WebClient();
+            HtmlDocument htmlDoc = new HtmlDocument();
+
+            byte[] page = client.DownloadData(URL);
+            string htmlPage = Encoding.UTF8.GetString(page);
+            htmlDoc.LoadHtml(htmlPage);
+
+            return htmlDoc;
+        }
+        static public Image GetImage(string URL)
+        {
+            WebClient client = new WebClient();
+            byte[] image = client.DownloadData(URL);
+            using (MemoryStream ms = new MemoryStream(image, 0, image.Length))
+            {
+                return Image.FromStream(ms, true);
+            }
+        }
+    }
+}
