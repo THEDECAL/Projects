57d45189e7f49e6ca09010ec6a344da8f5c83a2b
GEEK-PC <thedecal2@gmail.com>
2019-03-13 07:07:10 +0200

    13 мар 2019 г.  7:07:09

--- CSharp/BookShop/BookInfoControl.Designer.cs
+++ CSharp/BookShop/BookInfoControl.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.gbDesc = new System.Windows.Forms.GroupBox();
+            this.lbAuthor = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbDesc.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -43,11 +46,33 @@
             // 
             // gbDesc
             // 
+            this.gbDesc.Controls.Add(this.textBox1);
+            this.gbDesc.Controls.Add(this.lbAuthor);
             this.gbDesc.Location = new System.Drawing.Point(227, 12);
             this.gbDesc.Name = "gbDesc";
             this.gbDesc.Size = new System.Drawing.Size(200, 270);
             this.gbDesc.TabIndex = 1;
             this.gbDesc.TabStop = false;
+            // 
+            // lbAuthor
+            // 
+            this.lbAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbAuthor.Location = new System.Drawing.Point(6, 9);
+            this.lbAuthor.Name = "lbAuthor";
+            this.lbAuthor.Size = new System.Drawing.Size(188, 23);
+            this.lbAuthor.TabIndex = 0;
+            this.lbAuthor.Text = "Автор";
+            this.lbAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(9, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 45);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // BookInfoControl
             // 
@@ -59,6 +84,8 @@
             this.Name = "BookInfoControl";
             this.Text = "BookInfoControl";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbDesc.ResumeLayout(false);
+            this.gbDesc.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -67,5 +94,7 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox gbDesc;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbAuthor;
     }
 }
