a3898027c95ae506d8c3f4d57851ea3c36e1ec0e
GEEK-PC <thedecal2@gmail.com>
2019-03-25 16:54:31 +0200

    25 мар 2019 г. 16:54:31

---
+++ CSharp/WPFPhoneBook/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPFPhoneBook
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public People People { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = People;
+            this.IsMinButtonEnabled = false;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            gMain.Visibility = Visibility.Hidden;
+        }
+    }
+}
